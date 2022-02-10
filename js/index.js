4890e3a85d8c776ad8723b821b1615c4860fcd14
shakhawath <m.shakhawath18@gmail.com>
2022-02-10 15:31:22 +0600

    login page updated

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// get button
+document.getElementById('login-btn').addEventListener('click', function () {
+    // get user email
+    const emailInput = document.getElementById('user-email');
+    const userEnail = emailInput.value;
+    // get user password
+    const passwordInput = document.getElementById('user-password');
+    const userPassword = passwordInput.value;
+    // validation
+    if (userEnail == 'matir@bank.com' && userPassword == 1234) {
+        window.location.href = 'banking.html'
+    }
+    else {
+        'Please enter the correct email and password'
+    }
+})
